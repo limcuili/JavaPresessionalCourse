2deed5bbe98d8a919fa55d89070b3ad4142722f2
limcuili <limcuili@hotmail.com>
2018-09-26 11:55:52 +0100

    Requires 3 arguments for HowAreYou

--- HowAreYou/HowAreYou.java
+++ HowAreYou/HowAreYou.java
@@ -2,7 +2,9 @@ class HowAreYou {
 
     public static void main (String[] args) {
         System.out.print("Hello ");
-        System.out.print(args[0]);
+        System.out.print(args[0] + ", ");
+        System.out.print(args[1] + " and ");
+        System.out.print(args[2]);
         System.out.println(", how are you today?");
     }
 
@@ -12,5 +14,4 @@ class HowAreYou {
 To run the above in the terminal,
 compile with 'javac HowAreYou.java',
 run 'java HowAreYou Ellen Diana Tristan'.
-Observe that it takes your arguments as an array.
 */
