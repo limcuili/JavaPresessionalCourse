52cbfaf5b18b7044d8898335f677c224409f9521
limcuili <limcuili@hotmail.com>
2018-09-26 11:53:08 +0100

    Taking in command line arguments

---
+++ HowAreYou/HowAreYou.java
@@ -0,0 +1,16 @@
+class HowAreYou {
+
+    public static void main (String[] args) {
+        System.out.print("Hello ");
+        System.out.print(args[0]);
+        System.out.println(", how are you today?");
+    }
+
+}
+
+/*
+To run the above in the terminal,
+compile with 'javac HowAreYou.java',
+run 'java HowAreYou Ellen Diana Tristan'.
+Observe that it takes your arguments as an array.
+*/
