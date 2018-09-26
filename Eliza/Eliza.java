684e7f9de35ed9e9a13438a2ff23bdb3ad974587
limcuili <limcuili@hotmail.com>
2018-09-26 12:20:44 +0100

    corrected Eliza comments and added elseif

--- Eliza/Eliza.java
+++ Eliza/Eliza.java
@@ -9,6 +9,9 @@ class Eliza {
             System.out.println("It is excellent that you are feeling well.");
         } else if ( args[1].equals("bad") ) {
             System.out.println("I am sorry to hear that you are not well.");
+        } else if ( args[1].equals("miserable") ) {
+            System.out.println("What do you call a square that's been in a car crash?");
+            System.out.println("... a WRECK-tangle!");
         } else {
             System.out.println("Sorry I did not understand you.");
         }
@@ -19,6 +22,7 @@ class Eliza {
 /*
 To run the above code, we require two arguments, a name and a feeling.
 compile with 'javac Eliza.java',
-run with 'java Tristan well',
-run with 'java Tristan bad'.
+run with 'java Eliza Tristan well',
+run with 'java Eliza Tristan bad',
+run with 'java Eliza Konrad miserable',
  */
